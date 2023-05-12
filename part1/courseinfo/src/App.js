c77b3272057043f4ff96f2851b40f40ef41b3025
Kohdy Nicholson <kohdy.nicholson@varian.com>
2023-05-12 14:17:49 +0300

    refact[1.5]: refactor information into one course object

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,6 +1,6 @@
 const Header = (props) => 
   <h1>
-    {props.course}
+    {props.courseName}
   </h1>
 
 const Part = (props) => 
@@ -18,29 +18,30 @@ const Total = (props) =>
   </p>
 
 const App = () => {
-  const course = 'Half Stack application development'
-
-  const parts =[
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
