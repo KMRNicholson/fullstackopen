f844abec5fa5dc45b25e84925872502343be911d
Kohdy Nicholson <kohdy.nicholson@varian.com>
2023-07-04 19:57:50 +0300

    refact[1.13]: use anecdote object to track votes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,43 +1,36 @@
 import { useState } from 'react'
 
+const Feedback = (props) =>
+  <div>
+    <button onClick={props.handleVote}>
+      vote
+    </button>
+    <button onClick={props.handleNext}>
+      next anecdote
+    </button>
+  </div>
+
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
+    { anecdote: 'If it hurts, do it more often.', votes: 0 },
+    { anecdote: 'Adding manpower to a late software project makes it later!', votes: 0 },
+    { anecdote: 'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', votes: 0 },
+    { anecdote: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', votes: 0 },
+    { anecdote: 'Premature optimization is the root of all evil.', votes: 0 },
+    { anecdote: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', votes: 0 },
+    { anecdote: 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.', votes: 0 },
+    { anecdote: 'The only way to go fast, is to go well.', votes: 0 }
   ]
 
-  // const [anecdoteState, setAnecdoteState] = useState([
-  //   { 
-  //     anecdote:'If it hurts, do it more often.',
-  //     votes: 0
-  //   },
-  //   'Adding manpower to a late software project makes it later!',
-  //   'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  //   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  //   'Premature optimization is the root of all evil.',
-  //   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-  //   'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-  //   'The only way to go fast, is to go well.'
-  // ])
-   
   const [selected, setSelected] = useState(0)
 
-  const handleClick = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+  const handleNext = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+  const handleVote = () => {console.log('voted!')}
 
   return (
     <div>
-      {anecdotes[selected]}
-      <div>
-        <button onClick={handleClick}>
-          next anecdote
-        </button>
-      </div>
+      {anecdotes[selected].anecdote}
+      <Feedback handleNext={handleNext} handleVote={handleVote}/>
     </div>
   )
 }
