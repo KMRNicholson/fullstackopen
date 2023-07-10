8c55e1299c76b9b0992af0c596d50521b0ad14bc
Kohdy Nicholson <kohdy.nicholson@varian.com>
2023-07-10 17:55:21 +0300

    refact[2.5]: move Course into own file

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,28 +1,4 @@
-const Header = ({courseName}) => 
-  <h2>
-    {courseName}
-  </h2>
-
-const Part = ({name, exercises}) => 
-  <p>
-    {name} {exercises}
-  </p>
-
-const Content = ({parts}) => parts.map(part => 
-  <Part key={part.id} name={part.name} exercises={part.exercises}/>
-)
-
-const Total = ({parts}) => 
-  <strong>
-    total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercise(s)
-  </strong>
-
-const Course = ({course}) =>
-  <div>
-    <Header courseName={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </div>
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
