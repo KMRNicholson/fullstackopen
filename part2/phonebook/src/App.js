0d73be43d9e43c9cc9e9bbcefd5dd0cf3410a18c
Kohdy Nicholson <kohdy.nicholson@varian.com>
2023-07-11 19:11:33 +0300

    feat[2.9]: add filter to displayed persons

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,10 +3,14 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -29,11 +33,20 @@ const App = () => {
   }
 
   const handleNameChange = (event) => setNewName(event.target.value) 
-  const handleNumberChange = (event) => setNewNumber(event.target.value) 
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setFilter(event.target.value)
+
+  const filtered = filter.length > 0 
+    ? persons.filter(person => person.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase()))
+    : persons
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with: <input onChange={handleFilterChange} value={filter}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input onChange={handleNameChange} value={newName}/>
@@ -46,7 +59,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <Person key={person.name} name={person.name} number={person.number}/>)}
+      {filtered.map(person => <Person key={person.id} name={person.name} number={person.number}/>)}
     </div>
   )
 }
