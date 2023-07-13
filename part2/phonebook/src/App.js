1a948568045e155c6e0ab38f1947dc7f7da5d507
Kohdy Nicholson <kohdy.nicholson@varian.com>
2023-07-13 18:01:49 +0300

    feat[2.13]: add personsService for handling backend communication

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
+
+import personsService from './services/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -12,31 +13,26 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    const personsUri = 'http://localhost:3001/persons'
-    axios
-      .get(personsUri)
-      .then(response => setPersons(response.data))
+    personsService
+      .getAll()
+      .then(initialPersons => setPersons(initialPersons))
   }, [])
 
   const handleSubmit = (event) => {
     event.preventDefault()
 
     const personExists = persons.filter(person => person.name === newName).length > 0
-
     if(personExists){
       alert(`${newName} is already added to phonebook`)
       return
     }
 
-    const personsUri = 'http://localhost:3001/persons'
     const newPerson = { name: newName, number: newNumber }
-    axios
-      .post(personsUri, newPerson)
-      .then(response => {
-        setPersons(persons.concat(response.data))
-        setNewName('')
-        setNewNumber('')
-      })
+    personsService
+      .create(newPerson)
+      .then(createdPerson => setPersons(persons.concat(createdPerson)))
+    setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => setNewName(event.target.value) 
