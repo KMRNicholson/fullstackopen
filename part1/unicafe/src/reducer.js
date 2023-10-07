06f6a24fa9911357fbc4b439fa3468bb450366b7
Kohdy Nicholson <kohdy.nicholson@varian.com>
2023-10-07 15:00:24 +0300

    test[6.1]: add reducer tests for feedback counter

---
+++ part1/unicafe/src/reducer.js
@@ -0,0 +1,38 @@
+const initialFeedback = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+  all: 0,
+  average: 0,
+  positive: '0%'
+}
+
+const counterReducer = (feedback = initialFeedback, action) => {
+  const newFeedback = { ...feedback }
+  switch (action.type) {
+    case 'good':
+      newFeedback.good++
+      break;
+
+    case 'neutral':
+      newFeedback.neutral++
+      break;
+
+    case 'bad':
+      newFeedback.bad++
+      break;
+  
+    default:
+      return newFeedback
+  }
+
+  newFeedback.all = newFeedback.good + newFeedback.bad + newFeedback.neutral
+  newFeedback.average = (newFeedback.good - newFeedback.bad) / newFeedback.all
+
+  const positive = newFeedback.good / newFeedback.all * 100
+  newFeedback.positive = `${positive}%`
+
+  return newFeedback
+}
+
+export default counterReducer
