a1d5cc3dd79f5910b5facd65df3ceae4412695ba
Kohdy Nicholson <kohdy.nicholson@varian.com>
2023-10-07 18:12:21 +0300

    feat[6.3]: add anecdoteReducer with vote and next actions

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import _ from 'lodash'
+import { useSelector, useDispatch } from 'react-redux'
+import { voteAnecdote, nextAnecdote } from './reducers/anecdoteReducer'
 
 const Header = ({ title }) =>
   <h1>
@@ -19,58 +21,19 @@ const Anecdote = ({ anecdote, handleVote, handleNext }) =>
   </div>
 
 const App = () => {
-  const anecdoteList = [
-    {
-      content: 'If it hurts, do it more often.',
-      votes: 0
-    },
-    {
-      content: 'Adding manpower to a late software project makes it later!',
-      votes: 0
-    },
-    {
-      content: 'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-      votes: 0
-    },
-    {
-      content: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-      votes: 0
-    },
-    {
-      content: 'Premature optimization is the root of all evil.',
-      votes: 0
-    },
-    {
-      content: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-      votes: 0
-    },
-    {
-      content: 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-      votes: 0
-    },
-    {
-      content: 'The only way to go fast, is to go well.',
-      votes: 0
-    }
-  ]
-
-  const rand = () => Math.floor(Math.random() * anecdotes.length)
-
-  const [anecdotes, setAnecdotes] = useState(anecdoteList)
-  const [anecdoteOfDay, setAnecdoteOfDay] = useState(anecdotes[rand()])
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state.anecdotes)
+  const anecdoteOfTheDay = useSelector(state => state.anecdoteOfTheDay)
 
   const getHighestVotes = () => _.head(_.orderBy(anecdotes, 'votes', 'desc'))
 
-  const handleNext = () => setAnecdoteOfDay(anecdotes[rand()])
-  const handleVote = anecdote => () => {
-    const newAnecdotes = anecdotes.map(anec => anec.content === anecdote.content ? { content: anec.content, votes: ++anec.votes } : anec)
-    setAnecdotes(newAnecdotes)
-  }
+  const handleNext = () => dispatch(nextAnecdote())
+  const handleVote = anecdote => () => dispatch(voteAnecdote(anecdote))
 
   return (
     <div>
       <Header title='Anecdote of the day'/>
-      <Anecdote anecdote={anecdoteOfDay} handleNext={handleNext} handleVote={handleVote} />
+      <Anecdote anecdote={anecdoteOfTheDay} handleNext={handleNext} handleVote={handleVote} />
       <Header title='Anecdote with most votes'/>
       <Anecdote anecdote={getHighestVotes()} handleNext={handleNext} handleVote={handleVote} />
     </div>
--- part1/anecdotes/src/reducers/anecdoteReducer.js
+++ part1/anecdotes/src/reducers/anecdoteReducer.js
@@ -1,5 +1,80 @@
-const voteAnecdote = () => {
-  const newVotes = [...votes]
-  newVotes[getSelectedVotes()] += 1
-  setVotes(newVotes)
+import _ from 'lodash'
+
+const anecdoteList = [ 
+  {
+    content: 'If it hurts, do it more often.',
+    votes: 0
+  },
+  {
+    content: 'Adding manpower to a late software project makes it later!',
+    votes: 0
+  },
+  {
+    content: 'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    votes: 0
+  },
+  {
+    content: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    votes: 0
+  },
+  {
+    content: 'Premature optimization is the root of all evil.',
+    votes: 0
+  },
+  {
+    content: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    votes: 0
+  },
+  {
+    content: 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    votes: 0
+  },
+  {
+    content: 'The only way to go fast, is to go well.',
+    votes: 0
+  }
+]
+
+const rand = () => Math.floor(Math.random() * anecdoteList.length)
+
+const initialState = {
+  anecdoteOfTheDay: anecdoteList[rand()],
+  anecdotes: anecdoteList
 }
+
+const anecdoteReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'VOTE':
+      return {
+        anecdoteOfTheDay: state.anecdoteOfTheDay.content === action.payload.content ? action.payload : state.anecdoteOfTheDay,
+        anecdotes: state.anecdotes.map(anecdote => anecdote.content === action.payload.content ? action.payload : anecdote)
+      }
+    
+    case 'NEXT':
+      return {
+        anecdoteOfTheDay: anecdoteList[rand()],
+        anecdotes: state.anecdotes
+      }
+  
+    default:
+      return state
+  }
+}
+
+export const voteAnecdote = anecdote => {
+  return {
+    type: 'VOTE',
+    payload: {
+      content: anecdote.content,
+      votes: ++anecdote.votes
+    }
+  }
+}
+
+export const nextAnecdote = () => {
+  return {
+    type: 'NEXT'
+  }
+}
+
+export default anecdoteReducer
