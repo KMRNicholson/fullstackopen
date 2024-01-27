b574252efb9f9063b5173a721169c4209e746571
Kohdy Nicholson <kohdy.nicholson@varian.com>
2024-01-27 17:22:42 +0200

    feat[8.14]: add parameter support to allBooks query

--- part8/backend/index.js
+++ part8/backend/index.js
@@ -65,18 +65,16 @@ const resolvers = {
       });
       
       if(args.author && args.genre){
-        return books.filter(book => book.author === args.author && book.genres.includes(args.genre))
+        return books.filter(book => book.author.name === args.author && book.genres.includes(args.genre))
       }
 
       if(args.author){
-        return books.filter(book => book.author === args.author)
+        return books.filter(book => book.author.name === args.author)
       }
 
       if(args.genre){
         return books.filter(book => book.genres.includes(args.genre))
       }
-
-
 
       return books;
     },
