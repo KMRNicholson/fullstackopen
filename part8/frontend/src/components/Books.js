6ec39c0d034d2c612b6e944edafd142f1aea34d8
Kohdy Nicholson <kohdy.nicholson@varian.com>
2024-01-28 19:19:18 +0200

    feat[8.19]: add feature to filter by genre

--- part8/frontend/src/components/Books.js
+++ part8/frontend/src/components/Books.js
@@ -1,9 +1,15 @@
 import { useQuery } from '@apollo/client'
+import _ from 'lodash'
 
 import { ALL_BOOKS } from '../graphql/queries'
+import { useState } from 'react'
 
 const Books = () => {
-  const result = useQuery(ALL_BOOKS)
+  const [genre, setGenre] = useState("")
+  
+  const result = useQuery(ALL_BOOKS, {
+    variables: { genre: genre }
+  })
 
   if(result.loading){
     return <div>loading...</div>
@@ -11,10 +17,11 @@ const Books = () => {
 
   const books = result.data.allBooks
 
+  const genres = _.uniq(books.reduce((list, book) => list.concat(book.genres), []))
+
   return (
     <div>
       <h2>books</h2>
-
       <table>
         <tbody>
           <tr>
@@ -22,13 +29,14 @@ const Books = () => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
+          {books.map((book) => (
+            <tr key={book.title}>
+              <td>{book.title}</td>
+              <td>{book.author.name}</td>
+              <td>{book.published}</td>
             </tr>
           ))}
+          {genres.map(genre => <button key={genre} onClick={()=>setGenre(genre)}>{genre}</button>)}<button onClick={()=>setGenre("")}>clear filter</button>
         </tbody>
       </table>
     </div>
