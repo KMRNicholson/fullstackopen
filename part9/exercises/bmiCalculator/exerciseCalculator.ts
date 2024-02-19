e697d3c7e3b6408b6e5fad7ef9a259761aef54b0
Kohdy Nicholson <kohdy.nicholson@varian.com>
2024-02-19 21:31:02 +0200

    feat[9.3]: add ability to pass cmdline arguments to exercise calculator

--- part9/exercises/bmiCalculator/exerciseCalculator.ts
+++ part9/exercises/bmiCalculator/exerciseCalculator.ts
@@ -8,6 +8,11 @@ interface ExerciseReport {
   average: number
 }
 
+interface ExerciseInput {
+  period: number[],
+  target: number
+}
+
 const ratings = [
   'bad',
   'okay',
@@ -44,4 +49,36 @@ const calculateExercises = (dailyHours: number[], targetAverage: number): Exerci
   }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+const parseExerciseInput = (args: string[]): ExerciseInput => {
+  if (args.length < 4) throw new Error('Too few arguments. Please enter hours for at least one day and target.');
+  
+  let period: number[] = [];
+  let i = 2;
+
+  const target = Number(args[i++]);
+  if (isNaN(target)) throw new Error('Target hours must be specified as a number.');
+
+  while (i < args.length) {
+    const dailyHours = Number(args[i]);
+    if(isNaN(dailyHours)) throw new Error('Daily hours in period must be specified as numbers.');
+    period = [...period, dailyHours];
+    i++;
+  }
+
+  return {
+    period,
+    target
+  }
+}
+
+try {
+  const { period, target } = parseExerciseInput(process.argv)
+  console.log(calculateExercises(period, target));
+} catch (error: unknown) {
+  let errorMsg = 'Something bad happened';
+  if (error instanceof Error) {
+    errorMsg += `\nError: ${error.message}` 
+  }
+
+  console.log(errorMsg)
+}
