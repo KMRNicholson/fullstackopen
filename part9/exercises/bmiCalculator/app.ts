9ff4f7edb467314a6650789a3399b92837b72993
Kohdy Nicholson <kohdy.nicholson@varian.com>
2024-02-20 16:22:57 +0200

    feat[9.7]: add exercises post route with validations around the number array type

--- part9/exercises/bmiCalculator/app.ts
+++ part9/exercises/bmiCalculator/app.ts
@@ -2,11 +2,15 @@ import express from 'express';
 const app = express();
 
 import bmiRouter from './controllers/bmi';
+import exercisesRouter from './controllers/exercises';
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
+app.use(express.json());
+
 app.use('/bmi', bmiRouter);
+app.use('/exercises', exercisesRouter);
 
 export default app;
