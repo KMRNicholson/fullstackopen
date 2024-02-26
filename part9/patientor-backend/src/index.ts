9957fd231a43c33ef2923927412725de8c4ea07a
Kohdy Nicholson <kohdy.nicholson@varian.com>
2024-02-26 12:48:53 +0200

    feat[9.10]: add /api/diagnoses endpoint to patientor backend

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,15 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+
+import diagnosesRouter from './routes/diagnoses';
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = 3000;
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
